1b70f7d0bfd15c239467809323d680e8bd89a2c0
mdmohiuddin10 <hmmohiuddinctg1997@gmail.com>
2023-11-07 17:23:10 +0600

    created requestFood collection

--- index.js
+++ index.js
@@ -32,35 +32,44 @@ async function run() {
 
 
     // food collection
-    app.post('/allfood', async(req, res)=>{
-        const newProduct = req.body;
-        console.log(newProduct);
-        const result = await foodCollection.insertOne(newProduct)
-        res.send(result)
+    app.post('/allfood', async (req, res) => {
+      const newProduct = req.body;
+      console.log(newProduct);
+      const result = await foodCollection.insertOne(newProduct)
+      res.send(result)
 
     })
 
-    app.get('/allfoods', async(req, res)=>{
-        const curser = foodCollection.find()
-        const result = await curser.toArray()
-        res.send(result)
+    app.get('/allfoods', async (req, res) => {
+      const curser = foodCollection.find()
+      const result = await curser.toArray()
+      res.send(result)
     })
 
-    app.get('/allfoods/:id', async(req, res)=>{
+    app.get('/allfoods/:id', async (req, res) => {
       const id = req.params.id;
-      const qurey = { _id: new ObjectId(id)}
+      const qurey = { _id: new ObjectId(id) }
       const result = await foodCollection.findOne(qurey)
       res.send(result)
-  })
+    })
 
-  // request collection
-  app.post('/requestFood', async(req, res)=>{
-    const newProduct = req.body;
-    console.log(newProduct);
-    const result = await requestCollection.insertOne(newProduct)
-    res.send(result)
+    // request collection
+    app.post('/requestFood', async (req, res) => {
+      const newProduct = req.body;
+      // console.log(newProduct);
+      const result = await requestCollection.insertOne(newProduct)
+      res.send(result)
+    })
 
-})
+    app.get('/requestFood', async (req, res) => {
+      console.log(req.query);
+      let query = {};
+      if (req.query?.userEmail) {
+        query = { email: req.query.userEmail }
+      }
+      const result = await requestCollection.find(query).toArray();
+      res.send(result)
+    })
 
 
 
@@ -75,10 +84,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/', (req, res)=>{
-    res.send('server is running')
+app.get('/', (req, res) => {
+  res.send('server is running')
 })
 
-app.listen(port, ()=>{
-    console.log(`food server is running on port ${port}`);
+app.listen(port, () => {
+  console.log(`food server is running on port ${port}`);
 })
